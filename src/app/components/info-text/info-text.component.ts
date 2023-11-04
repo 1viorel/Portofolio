889372a1949c0c31a9e032c7e6d74ecf792fec7a
Viorel <soituviorel@gmail.com>
2023-11-04 20:29:04 +0200

    text

--- src/app/components/info-text/info-text.component.ts
+++ src/app/components/info-text/info-text.component.ts
@@ -1,45 +1,8 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info-text',
   templateUrl: './info-text.component.html',
   styleUrls: ['./info-text.component.scss'],
 })
-export class InfoTextComponent implements OnInit {
-  private lastScrollY = 0;
-  private showImage = true; // Add a variable to control image visibility
-
-  constructor(private el: ElementRef) {}
-
-  ngOnInit() {
-    // Wait for the page to fully load and then remove the initial scaling
-    window.addEventListener('load', () => {
-      const zoomContainer =
-        this.el.nativeElement.querySelector('.zoom-container');
-      zoomContainer.style.transform = 'scale(1)';
-    });
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    const zoomContainer =
-      this.el.nativeElement.querySelector('.zoom-container');
-    const containerTop = zoomContainer.getBoundingClientRect().top;
-
-    // Add or remove the 'fade-out' class based on the scroll direction
-    if (window.scrollY > this.lastScrollY) {
-      // Scrolling down
-      zoomContainer.classList.add('fade-out');
-    } else {
-      // Scrolling up
-      zoomContainer.classList.remove('fade-out');
-    }
-
-    this.lastScrollY = window.scrollY;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.showImage = window.innerWidth > 768; // Adjust the width as needed
-  }
-}
+export class InfoTextComponent {}
