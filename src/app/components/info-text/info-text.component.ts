a4e035491a10cd93326996e7c17c9c3bc3eabc43
Viorel <soituviorel@gmail.com>
2023-11-04 22:24:13 +0200

    upg

--- src/app/components/info-text/info-text.component.ts
+++ src/app/components/info-text/info-text.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-info-text',
   templateUrl: './info-text.component.html',
   styleUrls: ['./info-text.component.scss'],
 })
-export class InfoTextComponent {}
+export class InfoTextComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const element = document.getElementById('home');
+
+    if (element) {
+      const scrollY = window.scrollY;
+      const maxScroll = 300; // Adjust this value to control the scroll distance at which the element becomes fully transparent
+      const opacity = Math.max(1 - scrollY / maxScroll, 0); // Calculate the new opacity
+
+      element.style.opacity = opacity.toString();
+    }
+  }
+}
