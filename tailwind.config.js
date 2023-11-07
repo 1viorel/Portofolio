25271822f6f22ced83ad6993cc94a4922acb4235
Viorel <soituviorel@gmail.com>
2023-11-07 13:27:03 +0200

    mobile support (footer is now fucked up)

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,17 @@ module.exports = {
       coolorange: '#ffc300',
       coolyellow: '#ffd60a'
     },
-    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {}
   },
   plugins: [
     require('flowbite/plugin'),
