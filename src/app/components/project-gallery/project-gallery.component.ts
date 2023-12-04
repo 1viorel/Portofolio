e957e657702a59f04e8aaa0293757fe3eba71eb4
Viorel <soituviorel@gmail.com>
2023-12-04 16:37:27 +0200

    awesome update

--- src/app/components/project-gallery/project-gallery.component.ts
+++ src/app/components/project-gallery/project-gallery.component.ts
@@ -7,33 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ProjectGalleryComponent {
   projects = [
-    'Project 1',
-    'Project 2',
-    'Project 3',
-    'Project 4',
-    'Project 5',
-    'Project 6',
-    'Project 7',
-    'Project 8',
-    'Project 9',
-    'Project 10',
-    'Project 11',
-    'Project 12',
-  ];
-
-  images = [
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg',
-    'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-12.jpg',
+    {
+      title: 'Webapp 1',
+      link: 'https://www.google.com',
+      description:
+        'This is a webapp This is a webapp This is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webapp',
+      image: 'assets/images/placeholder.png',
+    },
+    {
+      title: 'Webapp 2',
+      link: 'https://www.google.com',
+      description:
+        'This is a webapp This is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webappThis is a webapp',
+      image: 'assets/images/placeholder.png',
+    },
+    {
+      title: 'Webapp 3',
+      link: 'https://www.google.com',
+      description: 'This is a webapp',
+      image: 'assets/images/placeholder.png',
+    },
   ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,20 @@ module.exports = {
       coolyellow: '#ffd60a'
     },
     screens: {
-      'tablet': '640px',
+      'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
-      'md': '1024px',
-      'sm': '640px',
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
 
     },
     extend: {}
